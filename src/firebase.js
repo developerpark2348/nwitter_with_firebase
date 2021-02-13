00d1dec1492d09a7a5415eb9d6502d03436fe962
developerpark2348 <developerpark2348@gmail.com>
2021-02-13 21:10:27 +0900

    firebase 사용법
    1. Firebase 홈페이지에서 프로젝트를 생성한다.
    
    2. npm install -save firebase
    
    3 . firebase에서 프로젝트를 만든 후, 사용할 웹 링크를 고른 후 해당 밑의 script 설정 파일을 firebase.js파일을 만들어 해당 파일 안에 붙여 넣어준다.
    
    4. import firebase from "firebase/app";로 firebase.js파일내에 임폴트해온다.
    
    5. export default firebase.initializeApp(firebaseConfig); 로 export 한다.
    
    6. index파일내에 import firebase from "./firebase"; 로 임폴트해온다.

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCIWZeo04bHk_Bxcn10JuLvdiO-O8rGJMw",
+    authDomain: "nwitter-d7011.firebaseapp.com",
+    projectId: "nwitter-d7011",
+    storageBucket: "nwitter-d7011.appspot.com",
+    messagingSenderId: "834338910798",
+    appId: "1:834338910798:web:a63c94f5e8ceafc50b491f"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
