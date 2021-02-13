e9e5f69b45ee94fb190b0a1660161ee1ee7079d5
developerpark2348 <developerpark2348@gmail.com>
2021-02-13 21:30:35 +0900

    env파일 사용법
    https://nomadcoders.co/nwitter/lectures/1909 참고
    사용 이유 : gitignore파일내에 올려 key참조 및 url참조를 막기 위하여.

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIWZeo04bHk_Bxcn10JuLvdiO-O8rGJMw",
-    authDomain: "nwitter-d7011.firebaseapp.com",
-    projectId: "nwitter-d7011",
-    storageBucket: "nwitter-d7011.appspot.com",
-    messagingSenderId: "834338910798",
-    appId: "1:834338910798:web:a63c94f5e8ceafc50b491f"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_ID,
   };
 
 export default firebase.initializeApp(firebaseConfig);
