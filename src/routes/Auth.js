e80bb2f00b61bf4c808886113fd457fa2f5c9e4b
developerpark2348 <developerpark2348@gmail.com>
2021-02-14 16:46:27 +0900

    Auth 컴포넌트
    Auth 컴포넌트  Email및 password에 state지정해둠

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,46 @@
-import React from "react";
+import {React, useState} from "react";
 
-const Auth = () => 
-<span>Auth</span>;
+const Auth = () => {
+    const[email, setEmail] = useState("");
+    const[password, setpassword] = useState("");
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if(name === "email"){
+            setEmail(value)
+        } else if (name=="password"){
+            setpassword(value)
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+return(
+    <div>
+        <form onSubmit={onSubmit}>
+            <input 
+                name="email" 
+                type="text"
+                placeholder="Email" 
+                required 
+                value={email}
+                onChange={onChange}
+            />
+            <input 
+                name="password"
+                type="password" 
+                placeholder="Password" 
+                required 
+                value={password}
+                onChange={onChange}
+            />
+            <input type="submit" value="Log In"/>
+        </form>
+        <div>
+            <button>Continue with Goolge</button>
+            <button>Continue with Github</button>
+        </div>
+    </div>
+    )
+}
 
 export default Auth;
