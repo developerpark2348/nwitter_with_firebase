e692f62ce2fe618252fe1949033bef74ecbb9a65
developerpark2348 <developerpark2348@gmail.com>
2021-02-15 00:04:24 +0900

    로그아웃 (SignOut)
    authService.signOut();을 사용하여 로그아웃하는 기능과 함께 전 프로젝트에서 사용하였던 react-router-dom의 useHistory를 사용하여 history.push("/")를 사용하여 로그아웃 함수와 함께 / 링크로 돌아가도록 만듦.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,15 @@
 import React from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter =  ({ isLoggedIn }) => {
 
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn 
                 ?
@@ -14,11 +17,17 @@ const AppRouter =  ({ isLoggedIn }) => {
                     <Route exact path="/">
                         <Home />
                     </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
                 </> 
-                : 
+                :
+                <>
                     <Route exact path="/">
                         <Auth />
-                    </Route>}
+                    </Route>
+                </>
+                }
             </Switch>
         </Router>
     )
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,19 @@
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 import React from "react";
 
-const Profile = () => 
-<span>Profile</span>;
+const Profile = () => {
+
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
+    return(
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
 
 export default Profile;
