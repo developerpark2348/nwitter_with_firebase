0229a34cc68913b0a5be5653b02d8864d038c517
developerpark2348 <developerpark2348@gmail.com>
2021-02-14 14:08:14 +0900

    인증을 받기 위해 firebase내 authService.currentUser을 사용
    firebase 내 authService.currentUser
    => null이나, 사용자값을 출력함.
    즉, 로그인한 상태인지, 비로그인 상태인지 구별 가능.
    
    *여러가지를 export default ~하는방법과 달리 export const 함수명 = 사용할 함수~ 로 특정 함수만 export가능함.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,28 +1,26 @@
-import {React, useState} from "react";
+import React from "react";
 import {HashRouter as Router, Route, Switch} from "react-router-dom"
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter =  () => {
-
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter =  ({ isLoggedIn }) => {
 
     return (
-    <Router>
-        <Switch>
-            {isLoggedIn 
-            ?
-            <>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-            </> 
-            : 
-                <Route exact path="/">
-                    <Auth />
-                </Route>}
-        </Switch>
-    </Router>
+        <Router>
+            <Switch>
+                {isLoggedIn 
+                ?
+                <>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </> 
+                : 
+                    <Route exact path="/">
+                        <Auth />
+                    </Route>}
+            </Switch>
+        </Router>
     )
 
 }
