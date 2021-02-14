234f08e70c6e211a5d464f027682fdcc375a1c23
developerpark2348 <developerpark2348@gmail.com>
2021-02-14 23:11:32 +0900

    Error Check
    google, github을 누르면 뜨는 에러 체크용 커밋

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(()=>{
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false)
+      }
+      setInit(true)
+    });
+  }, []) // firebase가 실행되기전 위의 isLoggedIn이 실행되기에 항상 null(로그인 되지 않음 으로 계산되어 loading이 뜨지 않는 버그가 생김. onAuthStateChanged를 사용하여 user의 변화가 생길경우 출력해줌.
 
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn}/>
+      { init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
       <footer>&copy; Nwitter {new Date().getFullYear()} </footer>
     </>
   );
