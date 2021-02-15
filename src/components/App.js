abb39794156a1f5fd2109f6008d15ecea3e88d23
developerpark2348 <developerpark2348@gmail.com>
2021-02-15 22:15:20 +0900

    데이터 가져오는 형식 변경 (실시간 트윗 기능 구현)
    snapshot을 활용하여 데이터를 가져왔기에, 실시간으로 데이터가 업데이트됨.

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ function App() {
 
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
 
   useEffect(()=>{
     authService.onAuthStateChanged((user) => {
       if(user){
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false)
       }
@@ -20,7 +22,7 @@ function App() {
 
   return (
     <>
-      { init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+      { init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "Initializing..."}
       <footer>&copy; Nwitter {new Date().getFullYear()} </footer>
     </>
   );
