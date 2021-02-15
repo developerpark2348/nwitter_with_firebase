b8e569bd4f8cd56224f5a8ac94ac889f22b197ad
developerpark2348 <developerpark2348@gmail.com>
2021-02-15 21:05:24 +0900

    트윗 기능 구현함.
    * dbService.collection("db폴더명").get()
    => 여러가지 정보를 포함한 db의 내용을 가져옴.
    
    단, 정보를 불러오는 시간이 있기에 다른 함수에 만들어 async및 awiat를 써야함. (본문참조)
    
    * 위의 get한 것을 변수에 넣고
    변수.forEach(document => console.log(document.data()))
    를 하면 해당 get한 db폴더의 내용들이 출력됨.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
 
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async() => {
+        const dbNweets = await dbService.collection("nweets").get()
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev])
+        });
+    }
+
+    useEffect(()=>{
+        getNweets();
+    }, []);
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -16,7 +32,7 @@ const Home = () => {
     const onChange = (event) => {
         const { target: {value}} = event;
         setNweet(value);
-    }
+    };
 
 return(
     <div>
@@ -24,6 +40,13 @@ return(
             <input value={nweet} onChange={onChange} type="text" placeholder="무슨 생각 중이신가요?" maxLength={120} />
             <input type="submit" value ="Nweet" />
         </form>
+        <div>
+            {nweets.map(nweet => 
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+            )}
+        </div>
     </div>
     )
 }
