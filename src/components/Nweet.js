5bccda91ac7bfa4fb6d30122c4b3bb4d28bbcc7a
developerpark2348 <developerpark2348@gmail.com>
2021-02-15 23:37:55 +0900

    삭제 및 업데이트 기능 구현
    dbService.doc(`폴더명/${해당id}`).update({ 같은 파일명: 바꿀이름})
    => firebase db내 업데이트
    
    dbService.doc(`폴더명/'${해당id}`).delete();
    => firebase db내 삭제

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,16 +1,66 @@
-import { useState } from "react";
-import React from "react";
+import React, {useState} from "react";
+import { dbService } from "fbase";
 
-const Nweet = ({ nweetObj, isOwner }) => (
-    <div>
-        <h4>{nweetObj.text}</h4>
-        {isOwner && (
-            <> 
-                <button>Delete Nweet</button>
-                <button>Edit Nweet</button> 
+const Nweet = ({ nweetObj, isOwner }) => {
+
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm("정말로 이 nweet을 삭제하시겠습니까?");
+        console.log(ok)
+        if (ok){
+            await dbService.doc(`nweets/${nweetObj.id}`).delete();
+        }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`nweets/${nweetObj.id}`).update({
+            text:newNweet,
+        })
+        setEditing(false);
+    }
+
+    const onChange = (event) => {
+        const {target: { value }, } = event;
+        setNewNweet(value);
+    }
+
+    return(
+        <div>
+            { 
+            editing ?
+                <>
+                <form onSubmit={onSubmit}>
+                    <input 
+                    type="text" 
+                    placeholder="nweet을 편집하세요" 
+                    value={newNweet} 
+                    required
+                    onChange={onChange}
+                    />
+                    <input
+                    type="submit"
+                    value="업데이트 nweet"
+                    />
+                </form>
+                <button onClick={toggleEditing}>취소하기</button>
+                </>
+            :
+            <>
+                <h4>{nweetObj.text}</h4>
+                {isOwner && (
+                    <> 
+                        <button onClick={onDeleteClick}>Nweet 삭제하기</button>
+                        <button onClick={toggleEditing}>Nweet 편집하기</button> 
+                    </>
+                )}
             </>
-        )}
-    </div>
-);
+            }
+        </div>
+    )
+};
 
 export default Nweet;
