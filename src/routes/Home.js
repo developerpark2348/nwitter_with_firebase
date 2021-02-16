367c0dab8824065e772f273f9ded617fdf3335e6
developerpark2348 <developerpark2348@gmail.com>
2021-02-16 16:29:41 +0900

    이미지 삽입 후 제거 버튼 제작
    이미지를 삽입하고 해당 이미지를 미리보기 가능하게 만들고 제거버튼을 구현하여 해당 이미지를 제거할 수 있게 만들어둠.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ const Home = ({ userObj }) => {
 
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     useEffect(()=>{
         dbService.collection("nweets").onSnapshot(snapshot => {
@@ -30,12 +31,32 @@ const Home = ({ userObj }) => {
         const { target: {value}} = event;
         setNweet(value);
     };
+    const onFileChange = (event) => {
+        const {target:{files}} = event;
+        // event안의 target으로 들어가 안의 files를 가져오는것을 뜻함.
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget: {result}, } = finishedEvent;
+            // finishedEvent의 currentTarger의 reuslt를 불러옴.
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+    const onClearAttachment = () => setAttachment(null);
 
 return(
     <div>
         <form onSubmit={onSubmit}>
             <input value={nweet} onChange={onChange} type="text" placeholder="무슨 생각 중이신가요?" maxLength={120} />
+            <input type="file" accept="image/*" onChange={onFileChange}/>
             <input type="submit" value ="Nweet" />
+            { attachment && 
+            <div>
+                <img src={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachment}>업로드 취소하기</button>
+            </div>
+            }
         </form>
         <div>
             {nweets.map((nweet) => 
