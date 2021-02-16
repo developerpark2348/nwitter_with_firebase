eba7160087a2ae6b8b22c70cd28ee85b71e97da9
developerpark2348 <developerpark2348@gmail.com>
2021-02-16 19:30:35 +0900

    프로필에 자신의 이름을 변경하는 기능 구현
    userObj.displayName = 해당 유저의 이름
    
    userObj.updateProfile({
    바꿀 항목 ex)
    displayName: 스테이트명, 바꿀이름
    })

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,44 @@
 import { authService } from "fbase";
 import { useHistory } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
 
     const history = useHistory();
+
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            })
+        }
+    }
+
     return(
         <>
+        <form onSubmit={onSubmit}>
+            <input 
+            onChange={onChange}
+            type="text" 
+            placeholder="Display Name"
+            value={newDisplayName} 
+            />
+            <input type="submit" value="Update Profile" />
+        </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
